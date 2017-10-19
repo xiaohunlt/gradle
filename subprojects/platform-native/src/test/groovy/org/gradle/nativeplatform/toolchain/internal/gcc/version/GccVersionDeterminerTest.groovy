763b6d8d2f6350e3a36f8918ad25e822eaa1401b
Stefan Wolf <wolf@gradle.com>
2017-10-19 14:17:38 +0200

    Fix another test

--- subprojects/platform-native/src/test/groovy/org/gradle/nativeplatform/toolchain/internal/gcc/version/GccVersionDeterminerTest.groovy
+++ subprojects/platform-native/src/test/groovy/org/gradle/nativeplatform/toolchain/internal/gcc/version/GccVersionDeterminerTest.groovy
@@ -26,6 +26,8 @@ import org.gradle.util.VersionNumber
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.util.regex.Matcher
+
 @UsesNativeServices
 class GccVersionDeterminerTest extends Specification {
     def execActionFactory = Mock(ExecActionFactory)
@@ -210,7 +212,7 @@ End of search list.
     }
 
     def "parses gcc system includes"() {
-        def includes = ['/usr/local', '/usr/some/dir']
+        def includes = ['/usr/local', '/usr/some/dir'].collect { it.replaceAll('/', Matcher.quoteReplacement(File.separator))}
         expect:
         def result = output gcc4, gccVerboseOutput(includes), false
         result.systemIncludes*.path == includes
@@ -221,8 +223,8 @@ End of search list.
             '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.0.0/include',
             '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
             '/usr/include'
-        ]
-        def frameworks = ['/System/Library/Frameworks', '/Library/Frameworks']
+        ].collect { it.replaceAll('/', Matcher.quoteReplacement(File.separator))}
+        def frameworks = ['/System/Library/Frameworks', '/Library/Frameworks'].collect { it.replaceAll('/', Matcher.quoteReplacement(File.separator))}
         expect:
         def result = output clang, clangVerboseOutput(includes, frameworks), true
         result.systemIncludes*.path == includes
